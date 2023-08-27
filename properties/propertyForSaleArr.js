035b483ffab3fa7ebb38fc2a7da9bee6cc863c21
MastooraTurkmen <132576850+MastooraTurkmen@users.noreply.github.com>
2023-08-27 21:57:40 +0430

    Update propertyForSaleArr.js

--- properties/propertyForSaleArr.js
+++ properties/propertyForSaleArr.js
@@ -4,28 +4,28 @@ export const propertyForSaleArr = [
         priceGBP: 890000,
         roomsM2: [14, 18, 14, 10, 6],
         comment: 'Highly desirable location in stunning scenery!',
-        image: 'cottage.jpg'
+        image: 'https://i.postimg.cc/N0YgYpxv/cottage.jpg'
     },
     {
         propertyLocation: 'Wirral, Liverpool',
         priceGBP: 650000,
         roomsM2: [18, 16, 15, 14, 17, 19, 9, 8],
         comment: 'Astonishing view with a modern finish!',
-        image: 'desres.jpg'
+        image: 'https://i.postimg.cc/76wM38Mt/desres.jpg'
     },
     {
         propertyLocation: 'Beach, Brighton',
         priceGBP: 420000,
         roomsM2: [5],
         comment: 'Beautiful interior and a spacious room.',
-        image: 'hut.jpg'
+        image: 'https://i.postimg.cc/j2CfcsWc/hut.jpg'
     },
     {
         propertyLocation: 'Highlands, Scotland',
         priceGBP: 550000,
         roomsM2: [6, 12, 11, 5],
         comment: 'Lots of potential, snug, a must see!',
-        image: 'shed.jpg'
+        image: 'https://i.postimg.cc/TYLmW3vY/shed.jpg'
     }
 ]
 
